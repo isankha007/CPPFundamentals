3d5415b4980b5b28d971d955f4e2d190c95e5bdd
sankhadeep <sankhadeep2007@gmail.com>
2019-04-06 18:38:01 +0530

    Modified

--- src/MapTheClass.cpp
+++ src/MapTheClass.cpp
@@ -7,6 +7,16 @@ private:
 	string name;
 	int age;
 public:
+
+	Person():name(""),age(0){
+
+	}
+
+	Person(const Person &person){
+		name=person.name;
+		age=person.age;
+		cout<<"Copy Constractor"<<endl;
+	}
 	Person(string name,int age):
 	name(name),age(age){
 	}
@@ -19,6 +29,7 @@ int main(){
 	people[0]=Person("sankha",40);
 	people[1]=Person("Debdoot",28);
 	people[2]=Person("Shankar",28);
+	people.insert(make_pair(65,Person("Naba",33)));
 	for(map<int,Person>::iterator it=people.begin();it!=people.end();it++){
 		it->second.print();
 	}
