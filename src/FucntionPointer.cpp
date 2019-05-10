9d112d89f9f964d8e6d3ee14d11cf11902619ad8
sankhadeep <sankhadeep2007@gmail.com>
2019-05-10 07:04:34 +0530

    Virtual fuction added

--- src/FucntionPointer.cpp
+++ src/FucntionPointer.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+class Parents{
+private:
+	int one;
+public:
+	Parents():one(0){}
+	Parents(const Parents &other):one(0){
+		one=other.one;
+	}
+	virtual void print(){
+		cout<<"parents Called"<<endl;
+	}
+ virtual ~Parents(){}
+};
+class Child:public Parents{
+	int two;
+	void print(){
+		cout<<"child Called"<<endl;
+	}
+};
+
 int display(int *n){
 	cout<<"This fucntion get called:"<<*n<<endl;
 	return ++(*n);
@@ -12,4 +32,11 @@ int main(){
 	int l=10;
 	int k=fptr(&l);
 	cout<<"THe value K= "<<k<<" n :"<<l<<endl;
+	Child c;
+	Parents &p=c;
+	p.print();
+
+	Parents p1=Child();
+	p1.print();
+	return 0;
 }
