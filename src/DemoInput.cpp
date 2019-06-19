c0b9c70320be819df814094ac67b3b71c6fecdfd
sankhadeep <sankhadeep2007@gmail.com>
2019-06-19 08:36:36 +0530

    Test file added

---
+++ src/DemoInput.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<sstream>
+#include <string>
+using namespace std;
+int main(void) {
+//	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	while(1){
+	int n;
+	cout<<"Enter what to fetch command (1) or value(2) input\n"<<endl;
+	cin>>n;
+//	cin.ignore();
+	switch(n){
+	case 1: //command //3rd
+	{ 		string str1;
+			//getline(cin,str1);
+
+//	       int counter=1;
+//	       while(counter<=1){
+//			cout<<"Enter value input\n";
+//			cin>>str1;
+//			counter++;
+////			cin.ignore();
+//	       }
+//	       cin.ignore();
+			fflush(stdin);
+			getline(cin, str1);
+			cin.ignore();
+			fflush(stdin);
+		break;
+	}
+	case 2:
+	{
+//			char str2[100];
+//			printf("Enter command input\n");
+//			scanf("%s",str2);
+//			printf("%s\n",returnValue(n, str2));
+		break;
+	}
+	case 3:
+		exit(0);
+		break;
+	default:
+		printf("Enter Valid input\n");
+	  }
+	}
+
+	return EXIT_SUCCESS;
+}
